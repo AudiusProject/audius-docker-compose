5e0ca297a9f348634bcbfb19813c8aa4264091a7
Cheran Senthilkumar <cheran.v.senthil@gmail.com>
2022-04-13 10:54:39 -0500

    Fix more bugs

--- setup.sh
+++ setup.sh
@@ -27,10 +27,14 @@ cat <<EOF | sudo tee /dev/null >/etc/docker/daemon.json
 }
 EOF
 
+# allow current user to use docker without sudo
+sudo usermod -aG docker $USER
+
 # install docker-compose
 DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
 mkdir -p $DOCKER_CONFIG/cli-plugins
 curl -SL https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
 
 # create directories for volumes
 sudo mkdir -p /var/k8s
