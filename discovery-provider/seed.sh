261baa2011c2172594f544f4b15dc161b526acb6
Raymond Jacobson <ray@audius.co>
2022-05-09 23:51:11 -0700

    Wipe db

--- discovery-provider/seed.sh
+++ discovery-provider/seed.sh
@@ -7,13 +7,13 @@ then
   echo "Downloading $ENV database..."
   curl https://audius-pgdump.s3-us-west-2.amazonaws.com/discProvProduction.dump -O
   echo "Restoring $ENV database to $audius_db_url..."
-  pg_restore -d $audius_db_url discProvProduction.dump
+  pg_restore -d $audius_db_url --clean --if-exists discProvProduction.dump
 elif [ "$ENV" == 'stage' ]
 then
   echo "Downloading $ENV database..."
   curl https://audius-pgdump.s3-us-west-2.amazonaws.com/discProvStaging.dump -O
   echo "Restoring $ENV database to $audius_db_url..."
-  pg_restore -d $audius_db_url discProvStaging.dump
+  pg_restore -d $audius_db_url --clean --if-exists discProvStaging.dump
 else
   echo "Invalid env: $ENV"
   exit 1
