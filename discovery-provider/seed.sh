bee2f5298453051f8e8c1dda9e8d78a50945d083
Cheran Senthilkumar <cheran.v.senthil@gmail.com>
2022-05-10 14:34:30 +0530

    Minor fixes

--- discovery-provider/seed.sh
+++ discovery-provider/seed.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/bash
+set -e
 
 NETWORK=$1
 
-if [ "$NETWORK" == 'prod' ]; then
+if [[ "$NETWORK" == "prod" ]]; then
   echo "Downloading $NETWORK database..."
   curl https://audius-pgdump.s3-us-west-2.amazonaws.com/discProvProduction.dump -O
   echo "Restoring $NETWORK database to $audius_db_url..."
   pg_restore -d $audius_db_url --clean --if-exists --verbose discProvProduction.dump
-elif [ "$NETWORK" == 'stage' ]; then
+elif [[ "$NETWORK" == "stage" ]]; then
   echo "Downloading $NETWORK database..."
   curl https://audius-pgdump.s3-us-west-2.amazonaws.com/discProvStaging.dump -O
   echo "Restoring $NETWORK database to $audius_db_url..."
