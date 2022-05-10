83711ded87d71dd69857b2c97f37ce114d674396
Raymond Jacobson <ray@audius.co>
2022-05-10 00:09:07 -0700

    Use verbose restore

--- discovery-provider/seed.sh
+++ discovery-provider/seed.sh
@@ -7,13 +7,13 @@ then
   echo "Downloading $ENV database..."
   curl https://audius-pgdump.s3-us-west-2.amazonaws.com/discProvProduction.dump -O
   echo "Restoring $ENV database to $audius_db_url..."
-  pg_restore -d $audius_db_url --clean --if-exists discProvProduction.dump
+  pg_restore -d $audius_db_url --clean --if-exists --verbose discProvProduction.dump
 elif [ "$ENV" == 'stage' ]
 then
   echo "Downloading $ENV database..."
   curl https://audius-pgdump.s3-us-west-2.amazonaws.com/discProvStaging.dump -O
   echo "Restoring $ENV database to $audius_db_url..."
-  pg_restore -d $audius_db_url --clean --if-exists discProvStaging.dump
+  pg_restore -d $audius_db_url --clean --if-exists --verbose discProvStaging.dump
 else
   echo "Invalid env: $ENV"
   exit 1
