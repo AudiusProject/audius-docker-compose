1fbfcd6a8a227153948384fa45f4f3feb0a3dee9
Cheran Senthilkumar <cheran.v.senthil@gmail.com>
2022-05-10 14:12:26 +0530

    Rename ENV to NETWORK and use env file over args

--- discovery-provider/seed.sh
+++ discovery-provider/seed.sh
@@ -1,20 +1,18 @@
 #!/usr/bin/bash
 
-ENV=$1
+NETWORK=$1
 
-if [ "$ENV" == 'prod' ]
-then
-  echo "Downloading $ENV database..."
+if [ "$NETWORK" == 'prod' ]; then
+  echo "Downloading $NETWORK database..."
   curl https://audius-pgdump.s3-us-west-2.amazonaws.com/discProvProduction.dump -O
-  echo "Restoring $ENV database to $audius_db_url..."
+  echo "Restoring $NETWORK database to $audius_db_url..."
   pg_restore -d $audius_db_url --clean --if-exists --verbose discProvProduction.dump
-elif [ "$ENV" == 'stage' ]
-then
-  echo "Downloading $ENV database..."
+elif [ "$NETWORK" == 'stage' ]; then
+  echo "Downloading $NETWORK database..."
   curl https://audius-pgdump.s3-us-west-2.amazonaws.com/discProvStaging.dump -O
-  echo "Restoring $ENV database to $audius_db_url..."
+  echo "Restoring $NETWORK database to $audius_db_url..."
   pg_restore -d $audius_db_url --clean --if-exists --verbose discProvStaging.dump
 else
-  echo "Invalid env: $ENV"
+  echo "Invalid network: $NETWORK"
   exit 1
 fi
