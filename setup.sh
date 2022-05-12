f6b60937aae4d502cceb7314a97b8415e0153abd
Cheran <cheran.v.senthil@gmail.com>
2022-05-12 19:43:22 +0530

    Start docker

--- setup.sh
+++ setup.sh
@@ -16,6 +16,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 sudo apt-get update -y
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo systemctl start docker
 
 # limit log size in docker
 cat <<EOF | sudo tee /dev/null >/etc/docker/daemon.json
@@ -35,6 +36,7 @@ DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
 mkdir -p $DOCKER_CONFIG/cli-plugins
 curl -SL https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
 chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+
 
 # create directories for volumes
 sudo mkdir -p /var/k8s
