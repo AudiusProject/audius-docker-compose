f3836b57804f6d26aad572db20aa640fea4e0b32
Dheeraj Manjunath <dheerajmanju1@gmail.com>
2022-05-19 11:13:42 -0400

    Add auto upgrade prompt back in

--- setup.sh
+++ setup.sh
@@ -57,7 +57,11 @@ touch discovery-provider/.env
 
 # setup service
 if [[ "$1" != "" ]]; then
-	audius-cli auto-upgrade "$1" || true
+	read -p "Setup auto upgrade? [Y/n] " -n 1 -r
+	echo
+	if [[ "$REPLY" =~ ^([Yy]|)$ ]]; then
+		audius-cli auto-upgrade "$1"
+	fi
 
 	audius-cli set-config --required "$1"
 
