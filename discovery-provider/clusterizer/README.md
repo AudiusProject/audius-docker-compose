60d745b474a71c9e3235ab64a140d3d598d5f87a
Steve Perkins <stereosteve@gmail.com>
2022-08-10 16:18:22 -0400

    cleanup

--- discovery-provider/clusterizer/README.md
+++ discovery-provider/clusterizer/README.md
@@ -15,6 +15,21 @@ cat nats/generated.conf
 docker compose restart nats
 docker logs nats
 
-# soon nats should be up... so instead of restart, can send reload signal to pid 1:
+# if nats is up, can instead send reload signal to pid 1:
 docker compose exec nats nats-server --signal reload=1
+
+# publish some test values
+# you can run this on stage 2, 3, 5 and you'll see values print from peers
+docker exec clusterizer npm run pub
+
+
 ```
+
+## TODO
+
+* each server could have a different `authorization` set depending on when they run the `generate` command.
+  I suspect if stage2 is missing stage3 nkey it wont get messages from stage3... but need to test that.
+  If so... should use `nsc` tool to create + publish jwts instead of inlining to config.
+  This might make sense anyway since it'll require fewer nats reloads over time.
+* remove hardcoded test / admin accounts
+* build `clusterizer` docker container and push to dockerhub... maybe move out of this codebase.
--- discovery-provider/clusterizer/src/natsTestPublish.ts
+++ discovery-provider/clusterizer/src/natsTestPublish.ts
@@ -1,25 +1,29 @@
-import { connect, nkeyAuthenticator, StringCodec } from 'nats'
+import { connect, JSONCodec, nkeyAuthenticator, StringCodec } from 'nats'
 import { getConfig } from './config'
 
-const { codec, nkey } = getConfig()
+const { wallet, nkey } = getConfig()
 
 async function main() {
   const nc = await connect({
     servers: 'nats:4222',
     authenticator: nkeyAuthenticator(nkey.getSeed()),
   })
-  const sc = StringCodec()
+  const sc = JSONCodec()
 
   const sub = nc.subscribe('testing.one')
   ;(async () => {
     for await (const m of sub) {
-      console.log(`[${sub.getProcessed()}]: ${sc.decode(m.data)}`)
+      const idx = sub.getProcessed()
+      const data = sc.decode(m.data)
+      console.log(idx, data)
     }
     console.log('subscription closed')
   })()
 
-  nc.publish('testing.one', sc.encode('world'))
-  nc.publish('testing.one', sc.encode('again'))
+  nc.publish(
+    'testing.one',
+    sc.encode({ wallet, nkey: nkey.getPublicKey(), time: new Date() })
+  )
 }
 
 main()
