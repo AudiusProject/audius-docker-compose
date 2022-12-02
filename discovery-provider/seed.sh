b4bf1dd1727258f14d82fcf3b2f44a90da22d01f
Raymond Jacobson <raymondshujacobson8@gmail.com>
2022-12-02 10:01:25 -0800

    Fixup

--- discovery-provider/seed.sh
+++ discovery-provider/seed.sh
@@ -7,7 +7,7 @@ if [[ "$NETWORK" == "prod" ]]; then
   echo "Downloading $NETWORK database..."
   curl https://audius-pgdump.s3-us-west-2.amazonaws.com/discProvProduction.dump -O
   echo "Restoring $NETWORK database to $audius_db_url..."
-  pg_restore -d $audius_db_url --no-owner --no-privileges --clean --if-exists --verbose discProvProduction.dump
+  pg_restore -d $audius_db_url --username postgres --no-privileges --clean --if-exists --verbose discProvProduction.dump
 elif [[ "$NETWORK" == "stage" ]]; then
   echo "Downloading $NETWORK database..."
   curl https://audius-pgdump.s3-us-west-2.amazonaws.com/discProvStaging.dump -O
