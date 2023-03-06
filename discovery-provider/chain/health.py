cfe99b3677bf40bd9ef41fd59c1cbde923d05043
Alec Savvy <alecsavvy@outlook.com>
2023-03-06 15:30:34 -0700

    more logic

--- discovery-provider/chain/health.py
+++ discovery-provider/chain/health.py
@@ -6,19 +6,34 @@ rpc_url = "http://localhost:8545"
 health_res = requests.get(health_url)
 health_json = health_res.json()
 
-healthy = True
-
 health_check_node = health_json.entries["node-health"]
-health_check_description = health_check_node.description
-health_check_healthy = health_json.status == "Unhealthy"
-
-if health_check_healthy:
-    healthy = False
+description = health_check_node.description
+health_check_unhealthy = health_json.status == "Unhealthy"
 
 not_producing_msg = "The node stopped producing blocks"
-if health_check_healthy and not_producing_msg in health_check_description:
-    healthy = True
+stopped_producing = not_producing_msg in description
 
-if healthy:
-    exit(0)  # healthy
-exit(1)    # unhealthy
+snap_rpc = {'method': 'clique_getSnapshot', 'params': [], 'id': 1, 'jsonrpc': '2.0'}
+clique_snapshot_res = requests.post(rpc_url, data=snap_rpc).json().result
+
+self_rpc = {"method": "net_localAddress", "params": [], "id": 1, "jsonrpc": "2.0"}
+local_address_res = requests.post(rpc_url, data=self_rpc).json().result
+
+is_current_signer = local_address_res in clique_snapshot_res.signers
+
+# node is reporting healthy
+if not health_check_unhealthy:
+    exit(0)
+
+# node should be able to sign but stopped producing, restart
+if is_current_signer and health_check_unhealthy and stopped_producing:
+    exit(1)  # prompt restart
+
+# node is not a signer but is reporting unhealthy because it cant produce
+# this is technically healthy but all nodes start as miners
+if not is_current_signer and health_check_unhealthy and stopped_producing:
+    exit(0)
+
+# node is reporting unhealthy for some other reason, prompt a restart
+if health_check_unhealthy and not stopped_producing:
+    exit(1)
