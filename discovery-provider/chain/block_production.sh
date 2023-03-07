52818203ecf986c453469febd9d7380536997b2a
Alec Savvy <alecsavvy@outlook.com>
2023-03-07 10:10:17 -0700

    dont use gateway

--- discovery-provider/chain/block_production.sh
+++ discovery-provider/chain/block_production.sh
@@ -1,9 +1,9 @@
-res_1=$(curl -s --data '{"method":"eth_blockNumber","params":[],"id":1,"jsonrpc":"2.0"}' -H "Content-Type: application/json" -X POST https://acdc-gateway.audius.co/)
+res_1=$(curl -s --data '{"method":"eth_blockNumber","params":[],"id":1,"jsonrpc":"2.0"}' -H "Content-Type: application/json" -X POST localhost:8545)
 block_res_1=$( jq -r '.result' <<< "${res_1}")
 block_num_1=$(($block_res_1))
 echo $block_num_1
 sleep 5
-res_2=$(curl -s --data '{"method":"eth_blockNumber","params":[],"id":1,"jsonrpc":"2.0"}' -H "Content-Type: application/json" -X POST https://acdc-gateway.audius.co/)
+res_2=$(curl -s --data '{"method":"eth_blockNumber","params":[],"id":1,"jsonrpc":"2.0"}' -H "Content-Type: application/json" -X POST localhost:8545)
 block_res_2=$( jq -r '.result' <<< "${res_2}")
 block_num_2=$(($block_res_2))
 echo $block_num_2
