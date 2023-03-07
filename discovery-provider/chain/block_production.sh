bde8cbdd9f5f3fa43053712349e4a5dc9e2649e8
Alec Savvy <alecsavvy@outlook.com>
2023-03-07 12:20:41 -0700

    posix compliance

--- discovery-provider/chain/block_production.sh
+++ discovery-provider/chain/block_production.sh
@@ -1,15 +1,11 @@
-function blocknum() {
-    res=$(curl -s --data '{"method":"eth_blockNumber","params":[],"id":1,"jsonrpc":"2.0"}' -H "Content-Type: application/json" -X POST https://acdc-gateway.audius.co)
-    block_res=$( jq -r '.result' <<< "${res}")
-    block_num=$(($block_res))
-    echo $block_num
-}
-
-block_num_1=$(blocknum)
+#!/bin/sh
+res_1=$$(curl -s --data '{"method":"eth_blockNumber","params":[],"id":1,"jsonrpc":"2.0"}' -H "Content-Type: application/json" -X POST https://acdc-gateway.audius.co | jq -r '.result')
+block_num_1=$$res_1
 sleep 5
-block_num_2=$(blocknum)
+res_2=$$(curl -s --data '{"method":"eth_blockNumber","params":[],"id":1,"jsonrpc":"2.0"}' -H "Content-Type: application/json" -X POST https://acdc-gateway.audius.co | jq -r '.result')
+block_num_2=$$res_2
 
-    if (( $block_num_2 == $block_num_1)); then
-        echo "blocks not being produced, restarting"
-        exit 1
-    fi
+if [ "$$block_num_2" = "$$block_num_1" ]; then
+    echo "blocks not being produced, restarting"
+    exit 1
+fi
